d18e7d6396020496f9567f4392105ccc726ea461
Benjamin Grange <benjamin.grange@epitech.eu>
2019-03-06 14:47:19 +0000

    Move the dynamic gdt to `arch/x86/boot/gdt.c`.

--- kernel/arch/x86/boot/gdt.c
+++ kernel/arch/x86/boot/gdt.c
@@ -8,8 +8,21 @@
 \******************************************************************************/
 
 /*
-** Great Descriptor Table used at boot time.
+** Functions to manipulate the Great Descriptor Table (GDT).
+**
+** We have two GDTs: one made compile-time used during boot time, and an other
+** one dynamically generated when booting used all the way after that.
+**
+** The GDT cannot entierely be made at compile-time. The number of segments we
+** are going to need and their content aren't all known at that point.
+**
+** For exemple, each core in an SMP-context need its own TSS descriptor, but
+** the number of active core isn't a compile-time available information.
+**
+** Therefore, we use a static GDT at boot time, and later switch to a dynamic
+** one when we have all the information we need to generate it.
 */
+
 
 #include <arch/x86/gdt.h>
 
@@ -79,7 +92,7 @@ struct gdt_segment_descriptor boot_gdt[] = {
 		.granularity = 1,
 	),
 
-	/* 0x28: TSS selector */
+	/* 0x28: TSS selector (bootstrap CPU) */
 	NEW_GDT_SYSTEM_ENTRY(
 		0x00000000,
 		0x00000000,
@@ -103,3 +116,21 @@ struct gdt_fatptr const boot_gdt_fatptr = {
 	.limit = sizeof(boot_gdt) - 1,
 	.base = boot_gdt,
 };
+
+/*
+** A dynamically allocated GDT.
+**
+** For now, this dynamic GDT isn't used nor generated at all.
+** (the kernel isn't advanced enough)
+*/
+struct gdt_segment_descriptor *gdt;
+
+/*
+** The fat pointer describing the GDT.
+** It contains its address and its size (minus 1).
+**
+** This fat pointer is given in argument to the LGDT instruction when loading
+** the GDT.
+*/
+__aligned(16)
+struct gdt_fatptr gdt_fatptr;
