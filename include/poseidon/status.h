7dbd63d924da839847ed923a62ada37cec6e5ea4
Arignir <github@sigint.sh>
2019-04-04 20:23:51 +0100

    Update `status.h` to include a stringified version of each status.

--- include/poseidon/status.h
+++ include/poseidon/status.h
@@ -20,7 +20,7 @@
 ** Kernel error management.
 **
 ** This enum contains all the possible error-codes a kernel function can return.
-** Error values are always negative.
+** `OK` is zero and error values are strictly positive numbers.
 **
 ** Feel free to complete it according your needs.
 */
@@ -28,14 +28,11 @@ enum				status
 {
 	OK			= 0,
 
-	// Used to calibrate the next enum values. Should never be used.
-	__ERR_UNKNOWN		= CHAR_MIN,
-
 	/* Internal errors */
 	ERR_INTERNAL,			/* Internal / unspecified error. Keep this one for extrem cases. */
 	ERR_NOT_IMPLEMENTED,		/* Operation is not implemented */
 	ERR_NOT_SUPPORTED,		/* Operation is not supported */
-	ERR_NO_MEMORY,			/* Operation couldn't complete because it went out of memory */
+	ERR_OUT_OF_MEMORY,		/* Operation couldn't complete because it went out of memory */
 	ERR_INTERRUPTED,		/* Operation got interrupted */
 
 	/* Parameters errors */
@@ -52,20 +49,31 @@ enum				status
 	ERR_ACCESS_DENIED,		/* The operation couldn't be completed because it's missing privileges */
 	ERR_TARGET_BUSY,		/* The target of the operation is busy */
 	ERR_NO_MORE_ID,			/* There is no more ID available */
-
-	/* Mapping errors */
-	ERR_ALREADY_MAPPED,		/* The given virtual address is already mapped */
-	ERR_NOT_MAPPED,			/* The given virtual address is not mapped*/
-	ERR_CANT_MAP,			/* The mapping can't be done (reasons may be arch-dependent) */
-
-	/* Filesystem errors */
-	ERR_ALREADY_MOUNTED,		/* The given mountpoint is already taken */
-	ERR_BAD_DEVICE,			/* The filesystem failed to execute an important device-related operation */
-	ERR_NOT_DIRECTORY,		/* The given path isn't a directory */
-	ERR_NOT_REGULAR_FILE,		/* The given path isn't a regular file */
-	ERR_END_OF_DIRECTORY,		/* Reached end of directory */
 };
 
 typedef enum status	status_t;
+
+static char const * const status_str[] = {
+	[OK] = "OK",
+
+	[ERR_INTERNAL] = "ERR_INTERNAL",
+	[ERR_NOT_IMPLEMENTED] = "ERR_NOT_IMPLEMENTED",
+	[ERR_NOT_SUPPORTED] = "ERR_NOT_SUPPORTED",
+	[ERR_OUT_OF_MEMORY] = "ERR_OUT_OF_MEMORY",
+	[ERR_INTERRUPTED] = "ERR_INTERRUPTED",
+
+	[ERR_INVALID_ARGS] = "ERR_INVALID_ARGS",
+	[ERR_OUT_OF_RANGE] = "ERR_OUT_OF_RANGE",
+	[ERR_BAD_HANDLER] = "ERR_BAD_HANDLER",
+
+	[ERR_BAD_STATE] = "ERR_BAD_STATE",
+	[ERR_TIMED_OUT] = "ERR_TIMED_OUT",
+	[ERR_CANCELED] = "ERR_CANCELED",
+	[ERR_NOT_FOUND] = "ERR_NOT_FOUND",
+	[ERR_ALREADY_EXIST] = "ERR_ALREADY_EXIST",
+	[ERR_ACCESS_DENIED] = "ERR_ACCESS_DENIED",
+	[ERR_TARGET_BUSY] = "ERR_TARGET_BUSY",
+	[ERR_NO_MORE_ID] = "ERR_NO_MORE_ID",
+};
 
 #endif /* !_POSEIDON_STATUS_H_ */
